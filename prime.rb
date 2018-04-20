2587d22013bf971d76f2be53e87d9f603326e572
David Moore <hey@iamdavidmoore.com>
2018-04-20 02:43:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'math'
+
+def prime?(int)
+  i = 0
+  if int < 2
+    false
+  end
+  while i < int
+    if int % i == 0
+      false
+      i += 1
+    end
+  end
+  true
+end
